46fc30a149393096e58ee6466ccbdbec585ec268
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-10-23 00:24:07 -0300

    Chama factory de cartões de vacina na criação do animal

--- src/animal/AnimalFactory.java
+++ src/animal/AnimalFactory.java
@@ -2,16 +2,20 @@ package animal;
 
 import java.util.Date;
 
+import vacina.CartaoVacinaFactory;
+
 public class AnimalFactory {
 	//TODO validators
 	public static Bovino newBovino(double peso, String raca, Date dataNasc, Generos genero) {
 		long id = Animal.getProximoId();
-		return new Bovino(id, peso, raca, dataNasc, genero, null);
+		var cartaoVacina = CartaoVacinaFactory.newCartaoVacinaBovino();
+		return new Bovino(id, peso, raca, dataNasc, genero, cartaoVacina);
 	}
 	
 	public static Suino newSuino(double peso, String raca, Date dataNasc, Generos genero) {
 		long id = Animal.getProximoId();
-		return new Suino(id, peso, raca, dataNasc, genero, null);
+		var cartaoVacina = CartaoVacinaFactory.newCartaoVacinaSuino();
+		return new Suino(id, peso, raca, dataNasc, genero, cartaoVacina);
 	}
 
 }
